9533f8bcf5bf2737e04b09ebd2f7bf08237f7bff
Aflah 3100 <aflah3100@gmail.com>
2025-06-01 23:34:47 +0530

    Changed issue with app assets

--- lib/core/app_assets.dart
+++ lib/core/app_assets.dart
@@ -1,8 +1,7 @@
 class AppAssets {
-  static const loginBackgroundImage =
-      '/home/aflah/Desktop/office_book_app/assets/images/background_image.png';
+
   static const officeBookImage1 =
-      '/home/aflah/Desktop/office_book_app/assets/images/office_book_image_1.png';
+      'assets/images/office_book_image_1.png';
   static const officeBookImage2 =
-      '/home/aflah/Desktop/office_book_app/assets/images/office_book_image_2.png';
+      'assets/images/office_book_image_2.png';
 }
