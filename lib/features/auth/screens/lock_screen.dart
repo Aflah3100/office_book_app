9aede342b6f0a3001b288c7859696e769e7c6f81
Aflah 3100 <aflah3100@gmail.com>
2025-06-02 23:14:53 +0530

    added carousal slider in lock screen.

--- lib/features/auth/screens/lock_screen.dart
+++ lib/features/auth/screens/lock_screen.dart
@@ -1,10 +1,15 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:office_book_app/core/app_assets.dart';
 import 'package:office_book_app/core/app_colors.dart';
 import 'package:office_book_app/features/auth/widgets/lock_screen_widgets.dart';
 
 class LockScreen extends StatelessWidget {
-  const LockScreen({super.key});
+  LockScreen({super.key});
+  final List<String> carouselImages = [
+    AppAssets.officeBookImage1,
+    AppAssets.officeBookImage2,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,24 +35,38 @@ class LockScreen extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //Left-Side-Carousal-Image
+              //Left-Side-Carousel-Slider
               SizedBox(
                 width: screenWidth * 0.5,
                 height: screenHeight,
-                child: Center(
-                  child: SizedBox(
-                    width: screenWidth * 0.35,
-                    height: screenHeight * 0.65,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      child: Image.asset(
-                        AppAssets.officeBookImage2,
-                        fit: BoxFit.fill,
+                child: CarouselSlider.builder(
+                  itemCount: carouselImages.length,
+                  itemBuilder: (context, index, realIndex) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Image.asset(
+                          carouselImages[index],
+                          fit: BoxFit.cover,
+                          width: screenWidth * 0.35,
+                          height: screenHeight * 0.65,
+                        ),
                       ),
-                    ),
+                    );
+                  },
+                  options: CarouselOptions(
+                    height: screenHeight * 0.65,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    enlargeCenterPage: true,
+                    viewportFraction: 1,
+                    scrollDirection: Axis.horizontal,
                   ),
                 ),
               ),
+
               //Right-Side-Login-Container
               Expanded(
                 child: SizedBox(
@@ -55,9 +74,7 @@ class LockScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisSize: MainAxisSize.min,
-                    children: [
-                      LockScreenContainer()
-                    ],
+                    children: [LockScreenContainer()],
                   ),
                 ),
               ),
